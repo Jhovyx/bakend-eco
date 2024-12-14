55960b51576faeb7fe992214d9cd884439699d0c
hilderasho/ <hilderarce566@gmail.com>
2024-12-14 11:37:23 -0500

    mejora

--- src/pago-detalle/pago-detalle.module.ts
+++ src/pago-detalle/pago-detalle.module.ts
@@ -1,4 +1,3 @@
-
 import { Module } from '@nestjs/common';
 import { PagoDetalleService } from './pago-detalle.service';
 import { PagoDetalleController } from './pago-detalle.controller';
@@ -7,10 +6,4 @@ import { PagoDetalleController } from './pago-detalle.controller';
   controllers: [PagoDetalleController],
   providers: [PagoDetalleService],
 })
-export class PagoDetalleModule {
-  id: number;
-  numeroTarjeta: string;
-  nombreTarjeta: string;
-  fechaVencimiento: string;
-  codigoSeguridad: string;
-}
+export class PagoDetalleModule {}
